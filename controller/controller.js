840187f423e565bca12030d86dbf918d9962bf4f
Andree Monette <andreecmonette@gmail.com>
2014-04-25 23:44:38 -0400

    Test controllers

---
+++ controller/controller.js
@@ -0,0 +1,13 @@
+window.addEventListener("deviceorientation", function(event) {
+  document.getElementById("div1").innerHTML=event.alpha.toString();
+  document.getElementById("div2").innerHTML=event.beta.toString();
+  document.getElementById("div3").innerHTML=event.gamma.toString();
+  document.getElementById("div4").innerHTML=event.alpha.toString();
+  document.getElementById("div5").innerHTML=event.alpha.toString();
+  document.getElementById("div6").innerHTML=event.alpha.toString();
+  document.getElementById("div7").innerHTML=event.alpha.toString();
+  document.getElementById("div8").innerHTML=event.alpha.toString();
+ 
+
+}
+    
