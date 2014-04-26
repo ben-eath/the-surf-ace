dc2160c3089450ca97d46436ba33ba801eeae312
Robert Lord <robert@lord.io>
2014-04-26 03:29:37 -0400

    Add keyboard control to mock sock

--- frontend/app/class/sock.js
+++ frontend/app/class/sock.js
@@ -2,18 +2,49 @@
 ;(function(exports) {
 	var Sock = function() {
 		// this.socket = new WebSocket("ace");
+		var self = this;
+		window.onkeydown = function(e) {
+			var shark = self.data.sharks[0];
+			switch(e.which) {
+				case 38:
+					shark.depth = 1;
+					break;
+				case 40:
+					shark.depth = -1;
+					break;
+				case 37:
+					shark.direction = -1;
+					break;
+				case 39:
+					shark.direction = 1;
+					break;
+			}
+		};
+		window.onkeyup = function(e) {
+			var shark = self.data.sharks[0];
+			switch(e.which) {
+				case 38:
+				case 40:
+					shark.depth = 0;
+					break;
+				case 37:
+				case 39:
+					shark.direction = 0;
+					break;
+			}
+		};
 	};
 
 	Sock.prototype = {
 		data: {
 			sharks: [
 				{
-					direction: 1,
-					depth: 1
+					direction: 0,
+					depth: 0
 				},
 				{
-					direction: -1,
-					depth: -1
+					direction: 0,
+					depth: 0
 				},
 				{
 					direction: 0,
