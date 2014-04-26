620e5c67b2fbef045866002fe6f10cbaf638beb8
Robert Lord <robert@lord.io>
2014-04-26 01:30:01 -0400

    Switch code indentation to tabs

--- frontend/app/beneath.js
+++ frontend/app/beneath.js
@@ -1,17 +1,17 @@
 ;(function(exports) {
-  var Game = function() {
-    var width = $(window).width();
-    var height = $(window).height();
-    this.c = new Coquette(this, "canvas", width, height, "#000");
-    this.sock = new Sock(function(data) {
-      // callback on data
-    });
+	var Game = function() {
+		var width = $(window).width();
+		var height = $(window).height();
+		this.c = new Coquette(this, "canvas", width, height, "#000");
+		this.sock = new Sock(function(data) {
+			// callback on data
+		});
 
-    this.c.entities.create(Ocean, {size: { x:width, y:height }});
-    this.c.entities.create(Shark, {});
-  };
+		this.c.entities.create(Ocean, {size: { x:width, y:height }});
+		this.c.entities.create(Shark, {});
+	};
 
-  window.addEventListener('load', function() {
-    new Game();
-  });
+	window.addEventListener('load', function() {
+		new Game();
+	});
 })();
--- frontend/app/class/sock.js
+++ frontend/app/class/sock.js
@@ -1,7 +1,7 @@
 // socket input from the server
 ;(function(exports) {
-  var Sock = function(callback) {
-    this.callback = callback;
-  };
-  exports.Sock = Sock;
+	var Sock = function(callback) {
+		this.callback = callback;
+	};
+	exports.Sock = Sock;
 })(window);
