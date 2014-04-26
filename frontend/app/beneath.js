27a1f2d94882f9035c2df64e295fde74cbd72fa3
Robert Lord <robert@lord.io>
2014-04-26 02:46:22 -0400

    Add mock sock

--- frontend/app/beneath.js
+++ frontend/app/beneath.js
@@ -3,9 +3,7 @@
 		var width = $(window).width();
 		var height = $(window).height();
 		this.c = new Coquette(this, "canvas", width, height, "#000");
-		this.sock = new Sock(function(data) {
-			// callback on data
-		});
+		this.c.sock = new Sock();
 
 		this.c.entities.create(Ocean, {size: { x:width, y:height }});
 		this.c.entities.create(Shark, {});
--- frontend/app/class/sock.js
+++ frontend/app/class/sock.js
@@ -1,7 +1,32 @@
 // socket input from the server
 ;(function(exports) {
-	var Sock = function(callback) {
-		this.callback = callback;
+	var Sock = function() {
+		// this.socket = new WebSocket("ace");
+	};
+
+	Sock.prototype = {
+		data: {
+			sharks: [
+				{
+					direction: 1,
+					depth: 1
+				},
+				{
+					direction: -1,
+					depth: -1
+				},
+				{
+					direction: 0,
+					depth: 0
+				}
+			]
+		},
+		updateData: function(data) {
+			this.data = data;
+		},
+		getSharkData: function(sharkID) {
+			return this.data.sharks[sharkID];
+		}
 	};
 	exports.Sock = Sock;
 })(window);
