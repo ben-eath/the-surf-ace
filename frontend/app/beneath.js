a44642416273d55e6a012c5dcc709f7d48efa020
Robert Lord <robert@lord.io>
2014-04-26 01:26:17 -0400

    Add socket class, initialize proper objects

--- frontend/app/beneath.js
+++ frontend/app/beneath.js
@@ -1,38 +1,14 @@
 ;(function(exports) {
   var Game = function() {
-    this.c = new Coquette(this, "canvas", $(window).width(), $(window).height(), "#000");
-
-    // paramour
-    this.c.entities.create(Person, { center: { x:250, y:40 }, color:"#099" });
-
-    // player
-    this.c.entities.create(Person, { center: { x:256, y:110 }, color:"#f07",
-      update: function() {
-        if (this.c.inputter.isDown(this.c.inputter.UP_ARROW)) {
-          this.center.y -= 0.4;
-        }
-      },
-
-      collision: function(other) {
-        other.center.y = this.center.y; // follow the player
-      }
+    var width = $(window).width();
+    var height = $(window).height();
+    this.c = new Coquette(this, "canvas", width, height, "#000");
+    this.sock = new Sock(function(data) {
+      // callback on data
     });
-  };
 
-  var Person = function(game, settings) {
-    this.c = game.c;
-    for (var i in settings) {
-      this[i] = settings[i];
-    }
-
-    this.size = { x:9, y:9 };
-    this.draw = function(ctx) {
-      ctx.fillStyle = settings.color;
-      ctx.fillRect(this.center.x - this.size.x / 2,
-                   this.center.y - this.size.y / 2,
-                   this.size.x,
-                   this.size.y);
-    };
+    this.c.entities.create(Ocean, {size: { x:width, y:height }});
+    this.c.entities.create(Shark, {});
   };
 
   window.addEventListener('load', function() {
