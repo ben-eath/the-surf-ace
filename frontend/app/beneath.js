3385f27ace6dec466a81053bf76f54d4fad87529
Robert Lord <robert@lord.io>
2014-04-26 20:45:09 -0400

    Add simple dialogue draft

--- frontend/app/beneath.js
+++ frontend/app/beneath.js
@@ -10,7 +10,7 @@
 		this.c.entities.create(Ocean, {size: { x:width, y:height }});
 		this.c.entities.create(Surfer, {});
 
-		this.c.entities.create(Control, {center: {x:width/2, y:height/2}});
+		this.c.entities.create(Control, {center: {x:width/2, y:height/2}, size: {x:width, y:height}});
 	};
 
 	window.addEventListener('load', function() {
