6ce1453262a550a1cffab0828701c39b2083a5c6
Robert Lord <robert@lord.io>
2014-04-28 04:02:15 -0400

    Remove gradiant waves

--- frontend/app/beneath.js
+++ frontend/app/beneath.js
@@ -9,38 +9,6 @@
 
 		this.c.entities.create(Ocean, {size: { x:width, y:height }, center: {x: width / 2, y: height / 2}});
 
-		var waveHeight = height / 2.5;
-		var i;
-		for(i = 0; i * waveHeight < height + waveHeight; i++) {
-			this.c.entities.create(Wave, {
-				center: {x: width / 2, y: waveHeight * i},
-				size: { x:width, y:waveHeight },
-				maxY: height,
-				speed: 1,
-				zindex: -5
-			});
-		}
-		waveHeight = height / 3;
-		for(i = 0; i * waveHeight < height + waveHeight; i++) {
-			this.c.entities.create(Wave, {
-				center: {x: width / 2, y: waveHeight * i},
-				size: { x:width, y:waveHeight },
-				maxY: height,
-				speed: 1.5,
-				zindex: -30
-			});
-		}
-		waveHeight = height / 2;
-		for(i = 0; i * waveHeight < height + waveHeight; i++) {
-			this.c.entities.create(Wave, {
-				center: {x: width / 2, y: waveHeight * i},
-				size: { x:width, y:waveHeight },
-				maxY: height,
-				speed: 3,
-				zindex: -40
-			});
-		}
-
 		this.c.entities.create(Surfer, {});
 
 		this.c.entities.create(Control, {center: {x:width/2, y:height/2}, size: {x:width, y:height}});
